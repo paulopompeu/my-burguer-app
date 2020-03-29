d65e7ab563663bda3273e3dc37f7f05539ef3f34
Paulo Pompeu Toledo de Oliveira <paulopompeu@Paulos-MBP.attlocal.net>
2020-03-29 00:20:30 -0500

    controls

--- src/containers/BurguerBuilder/BurguerBuilder.js
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -65,6 +65,7 @@ class BurguerBuilder extends Component {
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngrdientHandler}
           disabled={disabledInfo}
+          price={this.state.totalPrice}
         />
       </Aux>
     )
