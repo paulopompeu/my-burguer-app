1c11f381f6e151604c15939bf7f0cfa61982ccfe
Paulo Pompeu Toledo de Oliveira <paulopompeu@Paulos-MBP.attlocal.net>
2020-03-28 22:20:22 -0500

    feature added: functions to add ingredients

--- src/containers/BurguerBuilder/BurguerBuilder.js
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -4,6 +4,13 @@ import BuildControls from '../../components/Burguer/BuildControls/BuildControls'
 
 import Burguer from '../../components/Burguer/Burguer'
 
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+}
+
 class BurguerBuilder extends Component {
   state = {
     ingredients: {
@@ -11,22 +18,31 @@ class BurguerBuilder extends Component {
       bacon: 0,
       cheese: 0,
       meat: 0
-    }
+    },
+    totalPrice: 4
   }
 
-//   addIngredientHandler = (type) => {
+  addIngredientHandler = type => {
+    const oldCount = this.state.ingredients[type]
+    const updatedCount = oldCount + 1
+    const updatedIngredients = {
+      ...this.state.ingredients
+    }
+    updatedIngredients[type] = updatedCount
+    const priceAdition = INGREDIENT_PRICES[type]
+    const oldPrice = this.state.totalPrice
+    const newPrice = oldPrice + priceAdition
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
+  }
 
-//   }
-
-//   removeIngrdientHandler = (type) => {
-
-//   }
+  removeIngrdientHandler = type => {}
 
   render() {
     return (
       <Aux>
         <Burguer ingredients={this.state.ingredients} />
-        <BuildControls />
+        <BuildControls ingredientAdded={this.addIngredientHandler}
+        />
       </Aux>
     )
   }
