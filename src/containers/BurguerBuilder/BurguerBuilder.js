07300904c0f5af2cf4575305ad40aa5c3e7f3a4e
Paulo Pompeu Toledo de Oliveira <paulopompeu@Paulos-MBP.attlocal.net>
2020-06-02 19:55:12 -0500

    design adjustments

--- src/containers/BurguerBuilder/BurguerBuilder.js
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -91,6 +91,7 @@ class BurguerBuilder extends Component {
       <Aux>
         <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
           <OrderSummary ingredients={this.state.ingredients}
+          price={this.state.totalPrice}
           purchaseCancelled={this.purchaseCancelHandler}
           purchaseContinued={this.purchaseContinueHandler}
           />
